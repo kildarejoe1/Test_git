9208356ac35c6c3031251f4b64f0c2042f0a25ec
henry morrin <henry.morrin@iii.com>
2020-01-07 11:45:58 +0000

    Updated Project with Jenkins Pipeline and unuttest

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/home")
 def home():
     return "This is home page!!!"
 
 
+@app.route("/hello/<username>")
+def hello(username):
+    return 'Why Hello %s!\n' % username
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
